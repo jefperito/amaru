58f77a8bf3b85dfa8a42dfe19d1acdebb1236138
Gabriel Acosta <acostadariogabriel@gmail.com>
2015-05-10 22:20:46 -0300

    Add tests

--- tests/test_fobject.py
+++ tests/test_fobject.py
@@ -17,4 +17,30 @@
 # You should have received a copy of the GNU General Public License
 # along with Amaru; If not, see <http://www.gnu.org/licenses/>.
 
-#import amaru  # lint:ok
+import unittest
+import os
+from amaru.core import fobject
+
+
+class FObjectTestCase(unittest.TestCase):
+
+    def setUp(self):
+        cdir = os.path.join(os.path.dirname(__file__))
+        self._filename = os.path.join(cdir, "for_test")
+
+    def test_read(self):
+        afile = fobject.FObject(self._filename)
+        expected = "aáÁ~ñañ^sdqwe123456787654\nggǵǵǵaáahhbóóooHO"
+        content = afile.read()
+        self.assertTrue(content, expected)
+
+    def test_write(self):
+        pass
+
+    def test_is_new(self):
+        afile = fobject.FObject()
+        self.assertTrue(afile.is_new)
+
+
+if __name__ == "__main__":
+    unittest.main()
